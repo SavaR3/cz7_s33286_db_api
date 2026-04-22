a49fb9057ede72307f00a6f1866f7a0c25b61aba
Sava Rachynskyi <s33286@pjwstk.edu.pl>
2026-04-22 11:45:38 +0200

    Initialize AppointmentsController

--- cz7_s33286_db_api/Controllers/AppointmentsController.cs
+++ cz7_s33286_db_api/Controllers/AppointmentsController.cs
@@ -1,6 +1,18 @@
-﻿namespace cz7_s33286_db_api.Controllers;
+﻿using cz7_s33286_db_api.Services;
+using Microsoft.AspNetCore.Mvc;
 
-public class AppointmentsController
+namespace cz7_s33286_db_api.Controllers;
+
+[Route ("api/[controller]")]
+[ApiController]
+public class AppointmentsController : ControllerBase
 {
+    private readonly IAppointsService _appointsService;
+
+    public AppointmentsController(IAppointsService appointsService)
+    {
+        _appointsService = appointsService;
+    }
+    
     
 }
---
+++ cz7_s33286_db_api/Services/IAppointsService.cs
@@ -0,0 +1,6 @@
+﻿namespace cz7_s33286_db_api.Services;
+
+public interface IAppointsService
+{
+    
+}
